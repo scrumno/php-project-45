aa0b49c1f693944200995551f23bbd2d5394c359
Youmeon <leavetobefree777@gmail.com>
2025-12-10 23:18:31 +1000

    SPACE WAAAAR

--- src/brain-calc.php
+++ src/brain-calc.php
@@ -8,38 +8,38 @@ use function cli\prompt;
 
 function runCalc(): void
 {
-	$name = greeting();
+    $name = greeting();
 
-	line('What is the result of the expression?');
+    line('What is the result of the expression?');
 
-	for ($i = 0; $i < 3; $i++) {
-		$numberOne = rand(1, 100);
-		$numberTwo = rand(1, 100);
-		$operations = ['+', '-', '*'];
+    for ($i = 0; $i < 3; $i++) {
+        $numberOne = rand(1, 100);
+        $numberTwo = rand(1, 100);
+        $operations = ['+', '-', '*'];
 
-		$operation = $operations[array_rand($operations)];
-		switch ($operation) {
-			case '+':
-				$correctAnswer = $numberOne + $numberTwo;
-				break;
-			case '-':
-				$correctAnswer = $numberOne - $numberTwo;
-				break;
-			case '*':
-				$correctAnswer = $numberOne * $numberTwo;
-				break;
-		}
-		line("Question: %s %s %s", $numberOne, $operation, $numberTwo);
+        $operation = $operations[array_rand($operations)];
+        switch ($operation) {
+            case '+':
+                $correctAnswer = $numberOne + $numberTwo;
+                break;
+            case '-':
+                $correctAnswer = $numberOne - $numberTwo;
+                break;
+            case '*':
+                $correctAnswer = $numberOne * $numberTwo;
+                break;
+        }
+        line("Question: %s %s %s", $numberOne, $operation, $numberTwo);
 
-		$answer = prompt("Your answer");
+        $answer = prompt("Your answer");
 
-		if ((string)$correctAnswer === $answer) {
-			line("Correct!");
-		} else {
-			line("'%s' is wrong answer ;(. Correct answer was '%s'.", $answer, $correctAnswer);
-			line("Let's try again, %s!", $name);
-			return;
-		}
-	}
-	line("Congratulations, %s!", $name);
+        if ((string)$correctAnswer === $answer) {
+            line("Correct!");
+        } else {
+            line("'%s' is wrong answer ;(. Correct answer was '%s'.", $answer, $correctAnswer);
+            line("Let's try again, %s!", $name);
+            return;
+        }
+    }
+    line("Congratulations, %s!", $name);
 }
